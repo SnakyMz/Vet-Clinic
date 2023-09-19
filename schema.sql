5e89d4590345bd79404850228ba3d4807704716b
SnakyMz <Soban.syeed@outlook.com>
2023-09-20 02:50:20 +0530

    Update schema

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
+CREATE DATABASE vet_clinic;
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+
+CREATE TABLE animals (id INT GENERATED ALWAYS AS IDENTITY,
+                                       name VARCHAR(20) NOT NULL,
+                                                        date_of_birth DATE NOT NULL,
+                                                                           escape_attempts INT, neutered BOOLEAN, weight_kg DECIMAL(5,2) NOT NULL,
+                                                                                                                                         PRIMARY KEY(id));
+
