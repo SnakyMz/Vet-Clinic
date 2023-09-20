1f561097829ec0ba5df4417038dda68456cc8477
SnakyMz <Soban.syeed@outlook.com>
2023-09-21 00:20:36 +0530

    Update queries

--- queries.sql
+++ queries.sql
@@ -1,44 +1,66 @@
-/*Queries that provide answers to the questions from all projects.*/
-SELECT *
-from animals where(name like '%mon');
+/* Queries that provide answers to the questions from all projects. */
+/* Day 1 */
+SELECT * from animals where(name like '%mon');
 
+SELECT name from animals where date_of_birth >= '2016-01-01' and date_of_birth <= '2019-12-31';
 
-SELECT name
-from animals
-where date_of_birth >= '2016-01-01'
-  and date_of_birth <= '2019-12-31';
+SELECT name from animals where neutered = true and escape_attempts < 3;
 
+SELECT date_of_birth from animals where name = 'Agumon' or name = 'Pikachu';
 
-SELECT name
-from animals
-where neutered = true
-  and escape_attempts < 3;
+SELECT name, escape_attempts from animals where weight_kg > 10.5;
 
+SELECT * from animals where neutered = true;
 
-SELECT date_of_birth
-from animals
-where name = 'Agumon'
-  or name = 'Pikachu';
+SELECT * from animals where name not in('Gabumon');
 
+SELECT * from animals where weight_kg >= 10.4 and weight_kg <= 17.3;
 
-SELECT name,
-       escape_attempts
-from animals
-where weight_kg > 10.5;
+/* Day 2 */ 
+BEGIN;
+UPDATE animals
+SET species = 'unspecified';
+SELECT * animals;
+ROLLBACK;
+SELECT * animals;
+END;
 
+BEGIN;
+UPDATE animals
+SET species = 'digimon' where name like '%mon';
+UPDATE animals
+SET species = 'pokemon' where species is NULL;
+SELECT * from animals;
+COMMIT;
+SELECT * from animals order by id;
 
-SELECT *
-from animals
-where neutered = true;
+BEGIN;
+DELETE from animals;
+SELECT * from animals;
+ROLLBACK;
+SELECT * from animals;
+END;
 
+BEGIN;
+DELETE from animals where date_of_birth > '2022-01-10';
+SAVEPOINT delete2022;
+UPDATE animals
+SET weight_kg = weight_kg * -1;
+ROLLBACK to delete2022;
+UPDATE animals
+SET weight_kg = weight_kg * -1 where weight_kg < 0;
+COMMIT;
 
-SELECT *
-from animals
-where name not in('Gabumon');
+SELECT COUNT(*) from animals;
 
+SELECT COUNT(*) from animals where escape_attempts = 0;
 
-SELECT *
-from animals
-where weight_kg >= 10.4
-  and weight_kg <= 17.3;
+SELECT AVG(weight_kg) from animals;
 
+SELECT neutered, MAX(escape_attempts) from animals group by neutered;
+
+SELECT species, MIN(weight_kg), MAX(weight_kg) from animals group by species;
+
+SELECT species, AVG(escape_attempts) from animals
+where date_of_birth between '1990-01-01' and '2000-12-12'
+group by species;
